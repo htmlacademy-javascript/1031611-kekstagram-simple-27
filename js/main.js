65ee98ec9d0e4cf4f846e3770ca834b9420c5f65
tonny-da-creater <toni.belovanov96@mail.ru>
2022-10-06 21:01:45 +0300

    добавил альтернативные предложенные функции

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function getRandomNumber(min, max) {
-  if (min <= max && min >= 0) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-  return NaN;
-}
+// function getRandomNumber(min, max) {
+//   if (min <= max && min >= 0) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+//   }
+//   return NaN;
+// }
 
-getRandomNumber(2, 20);
+// getRandomNumber(2, 20);
 
 
 function getLengthString(string, maxLength) {
@@ -16,3 +16,21 @@ function getLengthString(string, maxLength) {
 }
 
 getLengthString('test', 10);
+
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+getRandomPositiveInteger();
+
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+checkStringLength();
